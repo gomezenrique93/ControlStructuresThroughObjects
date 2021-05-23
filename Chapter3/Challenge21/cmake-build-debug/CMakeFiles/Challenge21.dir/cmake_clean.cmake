c3e5dacc6d86ea8ef08814545dad6d82ba56a1b5
Enrique Gomez <gomez.enrique4595@gmail.com>
2021-05-22 19:23:21 -0700

    Added folder for #21

---
+++ Chapter3/Challenge21/cmake-build-debug/CMakeFiles/Challenge21.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Challenge21.dir/main.cpp.obj"
+  "Challenge21.exe"
+  "Challenge21.exe.manifest"
+  "Challenge21.pdb"
+  "libChallenge21.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Challenge21.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
