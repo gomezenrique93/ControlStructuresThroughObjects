a018de0ce4dc3cced5a22f65fe744acb1c1bc283
Enrique Gomez <gomez.enrique4595@gmail.com>
2021-05-29 15:16:50 -0700

    Challenge15 Input Validation

---
+++ Chapter4/Challenge15/cmake-build-debug/CMakeFiles/Challenge15.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Challenge15.dir/main.cpp.obj"
+  "Challenge15.exe"
+  "Challenge15.exe.manifest"
+  "Challenge15.pdb"
+  "libChallenge15.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Challenge15.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
