02f0c9672f71e59d36f00b937e29d7e33e7e84a5
Enrique Gomez <gomez.enrique4595@gmail.com>
2021-05-31 22:12:39 -0700

    Finished Challenge15

--- Chapter4/Challenge15/main.cpp
+++ Chapter4/Challenge15/main.cpp
@@ -14,6 +14,7 @@ ship). Do not accept distances of less than 10 miles or more than 3,000 miles. T
 are the company’s minimum and maximum shipping distances.
 */
 #include <iostream>
+#include <iomanip>
 
 int main() {
     // Variable Section
@@ -25,19 +26,58 @@ int main() {
     rate2 = 2.20,
     rate3 = 3.70,
     rate4 = 4.80,
-    dist_traveled;
+    pckg_distance,
+    shpng_charges = 0.0;
 
-    // User Input Section
+    // Package Weight Input Section
     std::cout << "Enter the weight of your package in kilograms: ";
     std::cin >> pckg_weight;
 
-    // Input Validation Section
-    while (pckg_weight > MAX_WEIGHT) {
-       std::cout << "Please enter a value less than " << MAX_WEIGHT << " kilograms " << std::endl;
-       std::cout << "Enter the weight of your package in kilograms: ";
-       std::cin >> pckg_weight;
+    // Package Weight Validation Section
+    while (pckg_weight > MAX_WEIGHT || pckg_weight <= 0)
+    {
+        if (pckg_weight <= 0)
+        {
+            std::cout << "Please make sure that weight of your package is greater than 0 kilograms!! \n";
+            std::cout << "Enter the weight of your package in kilograms: ";
+            std::cin >> pckg_weight;
+        }
+        else if (pckg_weight > MAX_WEIGHT)
+        {
+            std::cout << "Please make sure that the weight of your package is no more than " << MAX_WEIGHT << " kilograms \n";
+            std::cout << "Enter the wight of your package in kilograms: ";
+            std::cin >> pckg_weight;
+        }
     }
 
+    // Package Distance Input Section
+    std::cout << "Enter the distance, in miles that you want to send this package: ";
+    std::cin >> pckg_distance;
+
+    // Package Distance Validation Section
+    while (pckg_distance < DIST_MIN || pckg_distance > DIST_MAX)
+    {
+        std::cout << "Please make sure that the distance is between "
+                  << DIST_MIN << " miles and " << DIST_MAX << " miles. ";
+        std::cin >> pckg_distance;
+    }
+
+    // Algorithm for shpng_rate = rate * (pckg_distance / 500)
+    if (pckg_weight > 10 && pckg_weight <= 20) {
+        shpng_charges = rate4 * (pckg_distance / 500);
+    } else if (pckg_weight > 6 && pckg_weight <= 10) {
+        shpng_charges = rate3 * (pckg_distance / 500);
+    } else if (pckg_weight > 2 && pckg_weight <= 6) {
+        shpng_charges = rate2 * (pckg_distance / 500);
+    } else if (pckg_weight <= 2) {
+        shpng_charges = rate1 * (pckg_distance / 500);
+    }
+
+    // Shipping Charges Output Formatting Section
+    std::cout << std::fixed << std::showpoint << std::setprecision(2) << std::endl;
+
+    // Shipping Charges Output Section
+    std::cout << "Shipping charges for your package is $" << shpng_charges << std::endl;
 
     return 0;
 }
