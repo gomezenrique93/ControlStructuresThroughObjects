bbca66c81de46f626f1347813243460c77de8093
Enrique Gomez <gomez.enrique4595@gmail.com>
2021-05-31 22:13:02 -0700

    Added Folder for Challenge16

---
+++ Chapter4/Challenge16/cmake-build-debug/CMakeFiles/Challenge16.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Challenge16.dir/main.cpp.obj"
+  "Challenge16.exe"
+  "Challenge16.exe.manifest"
+  "Challenge16.pdb"
+  "libChallenge16.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Challenge16.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Chapter4/Challenge16/cmake-build-debug/CMakeFiles/Challenge16.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Challenge16.
+# This may be replaced when dependencies are built.
