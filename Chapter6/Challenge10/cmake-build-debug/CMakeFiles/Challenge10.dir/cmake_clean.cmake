1b9e25d384f50393703b284fe49060592a4a1720
Enrique Gomez <gomez.enrique4595@gmail.com>
2021-06-08 10:21:08 -0700

    Ch.6 Chall#10

---
+++ Chapter6/Challenge10/cmake-build-debug/CMakeFiles/Challenge10.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Challenge10.dir/main.cpp.o"
+  "Challenge10"
+  "Challenge10.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Challenge10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Chapter6/Challenge10/cmake-build-debug/CMakeFiles/Challenge10.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Challenge10.
+# This may be replaced when dependencies are built.
