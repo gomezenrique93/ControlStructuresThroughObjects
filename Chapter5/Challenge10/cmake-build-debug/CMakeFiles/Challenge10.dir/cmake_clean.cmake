59c5468657621599dfc354617e980e331b5f615b
Enrique Gomez <gomez.enrique4595@gmail.com>
2021-06-09 20:22:51 -0700

    Added Challenge10

---
+++ Chapter5/Challenge10/cmake-build-debug/CMakeFiles/Challenge10.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Challenge10.dir/main.cpp.obj"
+  "Challenge10.exe"
+  "Challenge10.exe.manifest"
+  "Challenge10.pdb"
+  "libChallenge10.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Challenge10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
