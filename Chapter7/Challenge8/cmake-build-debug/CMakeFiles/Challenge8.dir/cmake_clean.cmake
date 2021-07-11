011050d2a20d40dec243d87cb34bba122e3f8e15
Enrique Gomez <gomez.enrique4595@gmail.com>
2021-07-11 13:44:54 -0700

    Added Challenge8

---
+++ Chapter7/Challenge8/cmake-build-debug/CMakeFiles/Challenge8.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Challenge8.dir/main.cpp.obj"
+  "Challenge8.exe"
+  "Challenge8.exe.manifest"
+  "Challenge8.pdb"
+  "libChallenge8.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Challenge8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
