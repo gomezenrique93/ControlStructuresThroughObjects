2291dc1a9ad4ea8198a10a6135ca39a3679e0298
Enrique Gomez <gomez.enrique4595@gmail.com>
2022-06-27 19:03:45 -0700

    added Program9-1

---
+++ BookExamples/Program9-1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+
+	int x = 25;
+	std::cout << "The address of x is " << &x << std::endl;
+	std::cout << "The size of x is " << sizeof(x) << " byts\n";
+	std::cout << "The value in x is " << x << std::endl;
+	return 0;
+}
